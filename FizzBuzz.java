4cdf42db9c2ca40f8f7105012663c2dbc9f35c26
Jonatan_Mendoza <yonatanimm@gmail.com>
2021-02-15 08:45:23 +0200

    Fixes typo.

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class FizzBuzz {
   public static void main(final String [] args) {
-    System.out.println("Enter a integer positive number:");
+    System.out.println("Enter a positive integer:");
 
     final Scanner scanner =  new Scanner(System.in);
     final int number = scanner.nextInt();
